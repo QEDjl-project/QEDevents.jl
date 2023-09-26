7dbdd8b204d219e96f9a4366082a30f18007245c
Simeon Ehrig <s.ehrig@hzdr.de>
2023-09-26 09:43:44 +0200

    add code formatter job (#9)

--- docs/make.jl
+++ docs/make.jl
@@ -1,19 +1,17 @@
 using QEDevents
 using Documenter
 
-DocMeta.setdocmeta!(QEDevents, :DocTestSetup, :(using QEDevents); recursive=true)
+DocMeta.setdocmeta!(QEDevents, :DocTestSetup, :(using QEDevents); recursive = true)
 
 makedocs(;
-    modules=[QEDevents],
-    authors="Uwe Hernandez Acosta <u.hernandez@hzdr.de>, Simeon Ehrig, Klaus Steiniger, Tom Jungnickel, Anton Reinhard",
-    repo="https://github.com/QEDjl-project/QEDevents.jl/blob/{commit}{path}#{line}",
-    sitename="QEDevents.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        edit_link="main",
-        assets=String[],
+    modules = [QEDevents],
+    authors = "Uwe Hernandez Acosta <u.hernandez@hzdr.de>, Simeon Ehrig, Klaus Steiniger, Tom Jungnickel, Anton Reinhard",
+    repo = "https://github.com/QEDjl-project/QEDevents.jl/blob/{commit}{path}#{line}",
+    sitename = "QEDevents.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        edit_link = "main",
+        assets = String[],
     ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    pages = ["Home" => "index.md"],
 )
