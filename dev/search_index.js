c94d9ffafc59aa29f1cc2d1efe35bcdc0ae9a315
Documenter.jl <documenter@juliadocs.github.io>
2023-09-27 22:45:24 +0000

    build based on 36ec264

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = QEDevents","category":"page"},{"location":"#QEDevents","page":"Home","title":"QEDevents","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for QEDevents.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [QEDevents]","category":"page"}]
+}
