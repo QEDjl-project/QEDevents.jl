783c6d003e54703cf93ec44e8bb835e646974948
unknown <t.jungnickel@hzdr.de>
2023-10-12 11:25:37 +0200

    remove old test

--- test/interfaces.jl
+++ test/interfaces.jl
@@ -12,22 +12,6 @@ RTOL = sqrt(eps())
 
 RAND_MAX = rand(RNG)
 RAND_EXACTNESS = rand(RNG, Bool)
-#function _groundtruth_diffCS(x, y)
-#    return sum(x) * sum(y)
-#end
-
-#struct TestModel <: AbstractModelDefinition end
-#QEDcompton.fundamental_interaction_type(::TestModel) = :test_interaction
-
-#struct TestProcess <: AbstractProcessDefinition end
-#QEDcompton.incoming_particles(::TestProcess) = (:particle1, :particle2)
-#QEDcompton.outgoing_particles(::TestProcess) = (:particle3, :particle4)
-
-#struct TestProcessSetup{P,M,PS} <: AbstractProcessSetup
-#    proc::P
-#    model::M
-#    initPS::PS
-#end
 
 struct TestSetup{D} <: QEDevents.AbstractComputationSetup
     dist::D
@@ -42,17 +26,14 @@ end
 Base.size(stp::TestSetup,N::Integer) = size(stp.dist)[1] ###best way?
 Base.size(stp::TestSetup) = size(stp,1) #??
 
-#QEDcompton.scattering_process(stp::TestSamplerSetup) = stp.proc
-#QEDcompton.compute_model(stp::TestSamplerSetup) = stp.model
 QEDevents._compute(stp::TestSetup, x) = pdf(stp.dist, x)
 #Base.maximum(stp::TestSamplerSetup) = pdf(stp.dist, zeros(size(stp.dist)))
 
-#struct TestSampler{SFourMomentum} <: AbstractScatteringProcessSampler{SFourMomentum}
-#    stp::TestSetup
-#end
+
 struct TestSampler <: AbstractSampler
     stp::TestSetup
 end
+
 Base.size(samp::TestSampler,N::Integer) = size(samp.stp,N)
 Base.size(samp::TestSampler) = size(samp,1)
 
@@ -61,38 +42,12 @@ function QEDevents._rand!(rng::AbstractRNG, smplr::TestSampler, res::AbstractVec
     return res
 end
 
-#struct TestSampler{S<:TestSamplerSetup} <: AbstractScatteringProcessSampler{S}
-#    stp::S
-#end
 is_exact(::TestSampler) = RAND_EXACTNESS
 
 @testset "interface tests ($N_init, $N_final)" for (N_init, N_final) in Iterators.product(
     (1, rand(RNG, 2:8)), (1, rand(RNG, 2:8))
 )
     #=
-    QEDcompton.initial_phasespace_dimension(::TestProcess, ::TestModel) = N_init
-    QEDcompton.final_phasespace_dimension(::TestProcess, ::TestModel) = N_final
-
-    function QEDcompton._differential_cross_section(
-        ::TestProcess, ::TestModel, initPS::AbstractVector{T}, finalPS::AbstractVector{T}
-    ) where {T<:Real}
-        return _groundtruth_diffCS(initPS, finalPS)
-    end
-
-    function QEDcompton._total_cross_section(
-        ::TestProcess, ::TestModel, initPS::AbstractVector{T}
-    ) where {T<:Real}
-        return sum(initPS)
-    end
-
-    QEDcompton.scattering_process(stp::TestProcessSetup) = stp.proc
-    QEDcompton.compute_model(stp::TestProcessSetup) = stp.model
-    
-    function QEDcompton._compute(stp::TestProcessSetup, x)
-        # Fix me: this should be more general! 
-        # (maybe only possible, if the compute interface is similarly rich as the differential cross section interface) 
-        return QEDcompton._differential_cross_section(stp.proc, stp.model, stp.initPS, x)
-    end
     Base.maximum(stp::TestProcessSetup{P,M,PS}) where {P,M,PS<:AbstractVector} = RAND_MAX
     function Base.maximum(stp::TestProcessSetup{P,M,PS}) where {P,M,PS<:AbstractMatrix}
         return ones(eltype(stp.initPS), size(stp.initPS, 2))RAND_MAX
@@ -110,13 +65,13 @@ is_exact(::TestSampler) = RAND_EXACTNESS
 
 
     @testset "process sampler interface" begin
-        proc_stp = TestSetup(N_final)########
-        test_smplr = TestSampler(proc_stp)#TestSampler{SFourMomentum}(proc_stp)
+        proc_stp = TestSetup(N_final)
+        test_smplr = TestSampler(proc_stp)
 
         @testset "properties" begin
             @test setup(test_smplr) == proc_stp
             @test is_exact(test_smplr) == RAND_EXACTNESS
-            @test size(test_smplr) == size(proc_stp) #########
+            @test size(test_smplr) == size(proc_stp)
             #=@test isapprox(  #############################
                 max_weight(test_smplr),
                 pdf(proc_stp.dist, zeros(N_final)),
