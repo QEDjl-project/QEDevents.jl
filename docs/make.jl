51718f163958ce5700d1cc7be17b0a96adb9d669
unknown <t.jungnickel@hzdr.de>
2023-10-13 08:24:56 +0200

    fix format

--- docs/make.jl
+++ docs/make.jl
@@ -1,18 +1,16 @@
 using QEDevents
 using Documenter
 
-DocMeta.setdocmeta!(QEDevents, :DocTestSetup, :(using QEDevents); recursive = true)
+DocMeta.setdocmeta!(QEDevents, :DocTestSetup, :(using QEDevents); recursive=true)
 
 makedocs(;
-    modules = [QEDevents],
-    authors = "Uwe Hernandez Acosta <u.hernandez@hzdr.de>, Simeon Ehrig, Klaus Steiniger, Tom Jungnickel, Anton Reinhard",
-    repo = Documenter.Remotes.GitHub("QEDjl-project", "QEDevents.jl"),
-    sitename = "QEDevents.jl",
-    format = Documenter.HTML(;
-        prettyurls = get(ENV, "CI", "false") == "true",
-        edit_link = "main",
-        assets = String[],
+    modules=[QEDevents],
+    authors="Uwe Hernandez Acosta <u.hernandez@hzdr.de>, Simeon Ehrig, Klaus Steiniger, Tom Jungnickel, Anton Reinhard",
+    repo=Documenter.Remotes.GitHub("QEDjl-project", "QEDevents.jl"),
+    sitename="QEDevents.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true", edit_link="main", assets=String[]
     ),
-    pages = ["Home" => "index.md"],
+    pages=["Home" => "index.md"],
 )
-deploydocs(repo = "github.com/QEDjl-project/QEDevents.jl.git", push_preview = false)
+deploydocs(; repo="github.com/QEDjl-project/QEDevents.jl.git", push_preview=false)
