2ce30678b97b7433d300570444c5beac27ae8b8a
Tom Jungnickel <t.jungnickel@hzdr.de>
2023-10-16 11:23:07 +0200

    fixed last test

--- test/interfaces.jl
+++ test/interfaces.jl
@@ -25,7 +25,6 @@ Base.size(stp::TestSetup, N::Integer) = size(stp.dist)[1] ###best way?
 Base.size(stp::TestSetup) = size(stp, 1) #??
 
 QEDevents._compute(stp::TestSetup, x) = pdf(stp.dist, x)
-#Base.maximum(stp::TestSamplerSetup) = pdf(stp.dist, zeros(size(stp.dist)))
 
 struct TestSampler <: AbstractSampler
     stp::TestSetup
@@ -46,15 +45,9 @@ is_exact(::TestSampler) = RAND_EXACTNESS
 @testset "interface tests ($N_init, $N_final)" for (N_init, N_final) in Iterators.product(
     (1, rand(RNG, 2:8)), (1, rand(RNG, 2:8))
 )
-    #=
-    Base.maximum(stp::TestProcessSetup{P,M,PS}) where {P,M,PS<:AbstractVector} = RAND_MAX
-    function Base.maximum(stp::TestProcessSetup{P,M,PS}) where {P,M,PS<:AbstractMatrix}
-        return ones(eltype(stp.initPS), size(stp.initPS, 2))RAND_MAX
-    end
-    =#
     setup(smplr::TestSampler) = smplr.stp
     QEDevents._weight(smplr::TestSampler, x) = QEDevents.compute(smplr.stp, x) # assuming a product of Uniforms    ##remove the QEDevents nstead of replacing by processes because exported
-    max_weight(smplr::TestSampler) = maximum(smplr.stp.dist)
+    max_weight(smplr::TestSampler) = pdf(smplr.stp.dist, zeros(size(smplr.stp.dist)))
     function Distributions._rand!(
         rng::Random.AbstractRNG, s::TestSampler, x::AbstractVector{T}
     ) where {T<:Real}
@@ -70,12 +63,12 @@ is_exact(::TestSampler) = RAND_EXACTNESS
             @test setup(test_smplr) == proc_stp
             @test is_exact(test_smplr) == RAND_EXACTNESS
             @test size(test_smplr) == size(proc_stp)
-            #=@test isapprox(  #############################
+            @test isapprox(
                 max_weight(test_smplr),
                 pdf(proc_stp.dist, zeros(N_final)),
                 atol=ATOL,
                 rtol=RTOL,
-            )=#
+            )
         end
 
         @testset "weight: vector" begin
