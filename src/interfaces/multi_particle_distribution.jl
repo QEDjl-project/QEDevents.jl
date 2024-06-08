0868d9d68c42ed5420e42086245bebabb865bffe
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2024-06-08 22:15:26 +0200

    wip

--- src/interfaces/multi_particle_distribution.jl
+++ src/interfaces/multi_particle_distribution.jl
@@ -6,8 +6,7 @@
 Base type for sample drawing from multiple particle distributions. The following interface functions
 should be implemented:
 
-
-* [`QEDevents._particles(d::MultiParticleDistribution)`](@ref)
+* [`QEDevents._particles(d::MultiParticleDistribution)`](@ref): return
 * [`QEDevents._particle_directions(d::MultiParticleDistribution)`](@ref)
 
 """
@@ -15,51 +14,51 @@ const MultiParticleDistribution = ParticleSampleable{MultiParticleVariate}
 
 Broadcast.broadcastable(d::MultiParticleDistribution) = Ref(d)
 
-Base.length(::SingleParticleDistribution) = 1
-Base.size(::SingleParticleDistribution) = ()
+Base.length(d::MultiParticleDistribution) = length(_particles(d))
+Base.size(d::SingleParticleDistribution) = (length(d),)
 
 """
-    _particle(dist::SingleParticleDistribution)::QEDbase.AbstractParticle
+    _particle(dist::MultiParticleDistribution)
 
-Return the particle associated with the `dist`.
+Return tuple of particles associated with the `dist`.
 
 !!! note
 
-    Interface function to be implemented for single-particle distributions.
+    Interface function to be implemented for multi-particle distributions.
 
 """
-function _particle end
+function _particles end
 
 """
-    _particle_direction(dist::SingleParticleDistribution)::QEDbase.ParticleDirection
+    _particle_direction(dist::MultiParticleDistribution)
 
-Return the particle-direction of the particle associated with `dist`.
+Return tuple of particle-directions for all particles associated with `dist`.
+
 !!! note
 
-    Interface function to be implemented for single-particle distributions.
+    Interface function to be implemented for multi-particle distributions.
 
 """
-function _particle_direction end
+function _particle_directions end
 #default
-_particle_direction(::SingleParticleDistribution) = UnknownDirection()
+function _particle_direction(d::MultiParticleDistribution)
+    return Tuple(fill(UnknownDirection(), length(d)))
+end
 
 """
 Interface function, which asserts that the given `input` is valid.
 """
+#=
 function _assert_valid_input_type(
-    d::SingleParticleDistribution, x::ParticleStateful{D,P}
-) where {D,P}
-    eltype(_particle_direction(d)) == D ||
-        throw(InvalidInputError("expected $(_particle_direction(d)) but got $D"))
-
-    return eltype(_particle(d)) == P ||
-           throw(InvalidInputError("expected $(_particle(d)) but got $P"))
+    d::MultiParticleDistribution, x::PS
+) where {
+        PS <: Tuple{Vararg{ParticleStateful}
+    }
+    # TODO: implement correct type check
+    nothing
 end
+=#
 
 # used for pre-allocation of vectors of particle-stateful
-# todo: maybe find a better solution
-function Base.eltype(s::SingleParticleDistribution)
-    return ParticleStateful{
-        typeof(_particle_direction(s)),typeof(_particle(s)),_momentum_type(s)
-    }
-end
+# todo: use `_assemble_tuple_type` from QEDprocesses
+# function Base.eltype(s::MultiParticleDistribution) end
