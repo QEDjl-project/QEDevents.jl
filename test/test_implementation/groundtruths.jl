7feb6de8c281caf1cdc9e2039276d936678181ca
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2024-06-10 12:10:39 +0200

    cleanup

--- test/test_implementation/groundtruths.jl
+++
@@ -1,8 +0,0 @@
-_groundtruth_particle_type(dist) = TestParticle
-
-function _groundtruth_single_rand(rng, dist)
-    rnd_mom = rand(rng, SFourMomentum)
-    return ParticleStateful(
-        QEDevents._particle_direction(dist), QEDevents._particle(dist), rnd_mom
-    )
-end
