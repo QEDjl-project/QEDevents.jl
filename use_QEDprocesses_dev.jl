b0d80d0144deef3f4d5761dcca898e7ebed82177
Uwe Hernandez Acosta <szabo137@users.noreply.github.com>
2024-06-21 10:31:41 +0200

    Fix: particle implementations from core (#27)
    * remove use_QEDprocesses_dev.jl
    
    * fixed particle exports
    
    ---------
    
    Co-authored-by: Uwe Hernandez Acosta <u.hernandez@hzdr.de>

--- use_QEDprocesses_dev.jl
+++
@@ -1,5 +0,0 @@
-
-@warn "This repository depends on the dev branch of QEDprocesses.jl\n It is NOT ready for release!"
-
-using Pkg: Pkg
-Pkg.add(; url="https://github.com/QEDjl-project/QEDprocesses.jl", rev="dev")
