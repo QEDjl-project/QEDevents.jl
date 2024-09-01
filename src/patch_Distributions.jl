ff521400839be16b12e646eb1160956f8d465033
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2024-09-02 00:00:15 +0200

    added docstrings

--- src/patch_Distributions.jl
+++ src/patch_Distributions.jl
@@ -4,6 +4,26 @@
 const SQRT_TWO_OVER_PI = sqrt(2 / pi)
 const _CHI3 = Distributions.Chi(3; check_args=false)
 
+"""
+    MaxwellBoltzmann(scale::Real)
+
+
+The *Maxwell-Boltzmann distribution* with scale parameter `a` has the probability density function
+
+```math
+f(x,a) = \\sqrt{\\frac{2}{\\pi}}\\frac{x^2}{a^3}\\exp\\left(\\frac{-x^2}{2a^2}\\right)
+```
+
+The Maxwell-Boltzmann distribution is related to the `Chi` distribution via the property
+``X\\sim \\operatorname{MaxwellBoltzmann}(a=1)``, then ``X\\sim\\chi(\\mathrm{dof}=3)``.
+
+
+External links
+
+* [Maxwell-Boltzmann distribution on Wikipedia](https://en.wikipedia.org/wiki/Maxwell–Boltzmann_distribution)
+* [Maxwell-Boltzmann distribution on Wolfram MathWorld](https://mathworld.wolfram.com/MaxwellDistribution.html)
+* [Maxwell-Boltzmann distribution implementation in Scipy](https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.maxwell.html)
+"""
 struct MaxwellBoltzmann{T<:Real} <: Distributions.ContinuousUnivariateDistribution
     a::T # scale
     MaxwellBoltzmann{T}(a::T) where {T} = new{T}(a)
--- src/sampler/single_particle_dists/maxwell_boltzmann.jl
+++ src/sampler/single_particle_dists/maxwell_boltzmann.jl
@@ -1,3 +1,21 @@
+"""
+MaxwellBoltzmannParticle(
+    dir::ParticleDirection,
+    part::AbstractParticleType,
+    temperature::Real
+)
+
+The Maxwell-Boltzmann particle distribution is a single-particle distribution, where the
+spatial components of the four-momentum are normally distributed with localion ``\\mu = 0``
+and variance ``\\sigma = m k_B T`` (``m`` is the particle's mass, ``k_B`` is the Boltzmann constant,
+and ``T`` is the temperature). The three-magnitude ``\\varrho = \\sqrt{p_x^2 + p_y^2 + p_z^2}`` of the generated
+particle-statefuls is [`MaxwellBoltzmann`](@ref) distributed.
+
+External links
+
+* [Maxwell-Boltzmann distributed four-momenta on Wikipedia](https://en.wikipedia.org/wiki/Maxwell–Boltzmann_distribution#Distribution_for_the_momentum_vector)
+
+"""
 struct MaxwellBoltzmannParticle{D,P,T,DIST} <: SingleParticleDistribution
     dir::D
     part::P
@@ -18,7 +36,27 @@ _particle(d::MaxwellBoltzmannParticle) = d.part
 _particle_direction(d::MaxwellBoltzmannParticle) = d.dir
 temperature(d::MaxwellBoltzmannParticle) = d.temperature
 
-function QEDevents._weight(
+"""
+
+    _weight(
+        d::MaxwellBoltzmannParticle{D,P,T}, ps::ParticleStateful{D,P}
+    ) where {D,P,T}
+
+Unsafe weight-function for [`MaxwellBoltzmannParticle`](@ref), which is given by
+
+```math
+
+w(p) = \\begin{cases}
+\\frac{1}{4\\pi}\\sqrt{\\frac{2}{\\pi}}\\frac{\\varrho^2}{a^3}\\exp\\left(\\frac{-\\varrho^2}{2a^2}\\right)\\quad &\\text{,for } p^2=m^2\\\\
+0 &\\mathrm{elsewhere}.
+\\end{cases}
+
+```
+
+with ``\\varrho^2 = p_x^2 + p_y^2 + p_z^2`` and ``a = \\sqrt{m k_B T}`` (``m`` is the particle's mass, ``k_B`` is the Boltzmann constant,
+and ``T`` is the temperature).
+"""
+function _weight(
     d::MaxwellBoltzmannParticle{D,P,T}, ps::ParticleStateful{D,P}
 ) where {D,P,T}
     mom = momentum(ps)
